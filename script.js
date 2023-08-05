88b7e17a96690bc5ef3322b65517b2e0bbf7b196
Valentine <111288675+valxe@users.noreply.github.com>
2023-08-05 05:56:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-document.addEventListener("contextmenu", function(e) {
-    e.preventDefault();
-});
-
-document.addEventListener("touchstart", function(e) {
-    if (e.touches.length > 1) {
-        e.preventDefault();
-    }
-}, { passive: false });
-
 const randomNumberElement = document.getElementById("randomNumber");
 const generateButton = document.getElementById("generateButton");
 
