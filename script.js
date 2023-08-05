296297ea14d6dcce3b09c917b8578906825236ee
Valentine <111288675+valxe@users.noreply.github.com>
2023-08-05 13:41:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,11 +5,20 @@ const port = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+const messages = [];
+
 app.post('/receive-message', (req, res) => {
-  const { username, message } = req.body;
-  res.send('Message reçu hihi');
+  const { username, content } = req.body;
+  const newMessage = { username, content };
+  messages.push(newMessage);
+  console.log('Nouveau message reçu :', newMessage);
+  res.json({ message: 'Message reçu' });
+});
+
+app.get('/get-messages', (req, res) => {
+  res.json(messages);
 });
 
 app.listen(port, () => {
-  console.log(`C'est utile ca ${port}`);
+  console.log(`C'est utile je crois${port}`);
 });
