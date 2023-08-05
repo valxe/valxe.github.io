8dddcfb3a94ba0200fa9200fd046e412c758245f
Valentine <111288675+valxe@users.noreply.github.com>
2023-08-05 13:51:20 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 const messages = [];
 
-app.post('/receive-message', (req, res) => {
+app.post('/api/send-message', (req, res) => {
   const { username, content } = req.body;
   const newMessage = { username, content };
   messages.push(newMessage);
-  console.log('Nouveau message reçu :', newMessage);
+  console.log('Nouveau message :', newMessage);
   res.json({ message: 'Message reçu' });
 });
 
-app.get('/get-messages', (req, res) => {
+app.get('/api/get-messages', (req, res) => {
   res.json(messages);
 });
 
 app.listen(port, () => {
-  console.log(`C'est utile je crois${port}`);
+  console.log(`${port}`);
 });
