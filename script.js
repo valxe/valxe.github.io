6a4c6468f4c15a922c0f53aea6aadb4e15d6a075
Valentine <111288675+valxe@users.noreply.github.com>
2023-08-05 05:21:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const randomNumberElement = document.getElementById("randomNumber");
-const generateButton = document.getElementById("generateButton");
 
-generateButton.addEventListener("click", () => {
+window.addEventListener("load", () => {
     const randomNumber = Math.floor(Math.random() * 100) + 1; // Change 100 to your desired range
     randomNumberElement.textContent = randomNumber;
 });
