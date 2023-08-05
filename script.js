a807a0fe97534b7b1db3e9f040880062e1596c7e
Valentine <111288675+valxe@users.noreply.github.com>
2023-08-05 14:05:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
-const outputElement = document.querySelector('.output');
+const sendButton = document.getElementById('sendButton');
 
-function handleRequest(content, message) {
-  const requestInfo = document.createElement('p');
-  requestInfo.textContent = `Contenu : ${content}, Message : ${message}`;
-  outputElement.appendChild(requestInfo);
-}
+sendButton.addEventListener('click', async () => {
+  const content = document.getElementById('contentInput').value;
+  const message = document.getElementById('messageInput').value;
 
-const exampleContent = '';
-const exampleMessage = '';
-handleRequest(exampleContent, exampleMessage);
+  const formData = new FormData();
+  formData.append('content', content);
+  formData.append('message', message);
+
+  try {
+    const response = await fetch('https://formspree.io/f/xleygkzq', {
+      method: 'POST',
+      body: formData,
+    });
+
+    if (response.ok) {
+      console.log('Requête envoyée');
+    } else {
+      console.error('Échec');
+    }
+  } catch (error) {
+    console.error('Erreur:', error);
+  }
+});
